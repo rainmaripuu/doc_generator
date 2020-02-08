d9c9b190e0359bd900267d64104a2366ffda6679
Rain <rain.maripuu@gmail.com>
2020-02-08 10:59:53 +0200

    generating copy paste code

--- doc_gen.py
+++ doc_gen.py
@@ -1,3 +1,30 @@
 class DocGenInterface():
     def gen():
         pass
+
+class pdf(DocGenInterface):
+    def gen(self):
+        print('generating .pdf file')
+
+class word(DocGenInterface):
+    def gen(self):
+        print('generating .doc file')
+
+class DocGenFacotry():
+    @staticmethod
+    def get_gen(gen_str):
+        method = {
+            'word': word(),
+            'pdf': pdf()
+        }
+        gen = method.get(gen_str)
+        if gen == None:
+            assert 0, f'Cannot convert to <{gen_str}> format'
+        else:
+            return gen
+
+if __name__ == '__main__':
+    g = DocGenFacotry()
+    option = input('Enter document format: ')
+    gen_option = g.get_gen(option)
+    gen_option.gen()
